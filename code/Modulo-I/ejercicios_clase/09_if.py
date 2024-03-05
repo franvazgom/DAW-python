72fd97575d947aa5a77998287dd47575fb392688
franvazgomdev <franvazgom@gmnail.com>
2024-03-04 21:01:43 -0600

    if exersice added

---
+++ code/Modulo-I/ejercicios_clase/09_if.py
@@ -0,0 +1,13 @@
+
+def main():
+    name = input('Teclea tu nombre: ')
+    if name == 'Alice':
+        print("Hola Alice")
+        print("Bienvenida")
+    else:
+        print("Hola extrañ@")
+    print("Adios")
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/Modulo-I/ejercicios_clase/10_if.py
@@ -0,0 +1,11 @@
+
+def main():
+    name = input('Teclea tu nombre: ')
+    if name == 'Alice':
+        print("Hola Alice")
+        print("Bienvenida")
+    print("Adios")
+
+
+if __name__ == '__main__':
+    main()
