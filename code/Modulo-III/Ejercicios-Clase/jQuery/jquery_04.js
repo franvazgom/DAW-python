a40fe13742696a511c7d54ed5420f6c0c0b2d339
Francisco Vazquez <franvazgom@gmail.com>
2024-05-17 07:13:15 -0600

    Clase 16 de mayo 2024

---
+++ code/Modulo-III/Ejercicios-Clase/jQuery/jquery_04.js
@@ -0,0 +1,35 @@
+/* 
+Comentario multilinea
+*/
+$(function(){
+    $('#btn1').click(function(){
+        if ($(this).text() == "Cambia formato de table") {
+            $("#tablaNombres tr:odd").css('background', '#c1c1c1');
+            $(this).text("Elimina formato");
+        } else {
+            $("#tablaNombres tr:odd").css('background', 'white');
+            $(this).text("Cambia formato de table");
+        }
+    });
+    $('p').mouseenter(function(){
+        $(this).hide(1000);
+    });
+    $('p').mouseleave(function(){
+        $(this).show(1000);
+    });
+    $('#table2 tr').mouseover(function(){
+        $(this).css('background', 'yellow');
+    });
+    $('#table2 tr').mouseleave(function(){
+        $(this).css('background', 'white');
+    });
+    $("tr").dblclick(function(){
+        // $('#box1').val($(this).text());
+        $('#box1').val($(this).find("td").eq(-2).text());
+    });
+    cambiaTexto();
+});
+
+function cambiaTexto() {
+    $('#btnP').text("ABC"); 
+}
