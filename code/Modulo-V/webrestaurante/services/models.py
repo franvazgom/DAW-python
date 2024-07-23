3e4c993a247ea3d034bb21431f5a8f6bbbc6619e
Francisco Vazquez <franvazgom@gmail.com>
2024-07-22 20:51:37 -0600

    Clase 22 Julio 2024

--- code/Modulo-V/webrestaurante/services/models.py
+++ code/Modulo-V/webrestaurante/services/models.py
@@ -4,7 +4,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 class Service(models.Model):
     title = models.CharField(max_length=100, verbose_name='Título')
     subtitle = models.CharField(max_length=100, verbose_name='Subtítutlo')
-    content = CKEditor5Field(verbose_name='Contenido', config_name='extends')
+    content = CKEditor5Field('Contenido', config_name='extends', blank=True, null=True)
     image = models.ImageField(verbose_name='Imágen', upload_to='services')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
--- code/Modulo-V/webrestaurante/services/urls.py
+++ code/Modulo-V/webrestaurante/services/urls.py
@@ -3,4 +3,7 @@ from services import views
 
 services_urlpatterns = ([
     path('', views.service_list, name='service_list'),    
+    path('create/', views.create, name='create'), 
+    path('update/<int:service_id>', views.update, name='update'), 
+    path('delete/<int:service_id>', views.delete, name='delete'), 
 ], 'services')
