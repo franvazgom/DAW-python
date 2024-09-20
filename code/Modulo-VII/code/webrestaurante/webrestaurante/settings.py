6dddb0c9727e7fb54aa5817e77327c29add19cd4
Francisco Vazquez <franvazgom@gmail.com>
2024-09-19 20:05:38 -0600

    MySQL Material

--- code/Modulo-VII/code/webrestaurante/webrestaurante/settings.py
+++ code/Modulo-VII/code/webrestaurante/webrestaurante/settings.py
@@ -71,9 +71,17 @@ WSGI_APPLICATION = 'webrestaurante.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurante_db',
+        'USER': 'root_restaurante',
+        'PASSWORD': 'password14',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
